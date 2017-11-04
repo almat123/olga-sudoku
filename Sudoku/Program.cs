eb8a8953b5dfcfa69b6a9c59e678346b0c7fcbdd
Your Namealmat123 <you@example.comalmatvienko@rogers.com>
2017-11-04 18:14:04 -0400

    first commit

---
+++ Sudoku/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    public static class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            string path = Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.FullName, "input_sudoku.txt");
+            try
+            {
+                var lines = GetLinesFromFile(path);
+                int[,] grid = CreateIntGrid(lines);
+
+                var sudoku = new SudokuValidate(grid);
+                if (sudoku.ValidateRows() && sudoku.ValidateColumns() && sudoku.ValidateSquares())
+                    Console.WriteLine("Sudoku is valid");
+                else
+                    Console.WriteLine("Sudoku is not valid");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error reading from {0}. Message = {1}", path, e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: '{0}'", e);
+            }
+            Console.ReadLine();
+        }
+
+        public  static string[] GetLinesFromFile(string path)
+        {
+            string[] lines = System.IO.File.ReadAllLines(path);
+            return lines.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+        }
+
+        public static int[,] CreateIntGrid(string[] lines)
+        {
+            int[,] gridNumbers = new int[9, 9];
+            for (int i = 0; i < 9; ++i)
+            {
+                int[] lineNumbers = lines[i].ToCharArray().Select(x => (int)(x - '0')).ToArray();
+                for (int j = 0; j < 9; ++j)
+                {
+                    gridNumbers[i, j] = Convert.ToInt32(lineNumbers[j]);
+                }
+            }
+            return gridNumbers;
+        }
+
+    }
+
+    public class SudokuValidate
+    {
+        private int[,] grid;
+
+        public SudokuValidate(int[,] grid)
+        {
+            this.grid = grid;
+        }
+
+
+        public bool ValidateRows()
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                int[] row = Enumerable.Range(0, 9).Select(i => grid[j, i]).ToArray();
+                if (!IsValid(row))
+                    return false;
+            }
+            return true;
+        }
+
+        public bool ValidateColumns()
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                int[] column = Enumerable.Range(0, 9).Select(i => grid[i, j]).ToArray();
+                if (!IsValid(column))
+                    return false;
+            }
+            return true;
+        }
+
+        public  bool ValidateSquares()
+        {
+            var squares = GetSquares().ToArray();
+
+            for (int i = 0; i < 9; i++)
+            {
+                if (!IsValid(squares[i]))
+                     return false;
+            }
+            return true;
+        }
+
+        private IEnumerable<int[]> GetSquares()
+        {
+            return 
+                from i in Enumerable.Range(0, 3)
+                from j in Enumerable.Range(0, 3)
+                select GetSquare(i, j).ToArray();
+        }
+
+        private IEnumerable<int> GetSquare(int i, int j)
+        {
+            return
+                from x in Enumerable.Range(0, 3)
+                from y in Enumerable.Range(0, 3)
+                select grid[i * 3 + x, j * 3 + y];
+        }
+
+        public static bool IsValid(int[] values)
+        {
+            int flag = 0;
+            foreach (int value in values)
+            {
+                if (value != 0)
+                {
+                    int bit = 1 << value;
+                    if ((flag & bit) != 0) return false;
+                    flag |= bit;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ SudokuTest/ProgramTest.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sudoku;
+using System.IO;
+
+namespace SudokuTest
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        int[,] arrayValid = new int[9, 9]{
+        {5,3,4,6,7,8,9,1,2},
+        {6,7,2,1,9,5,3,4,8},
+        {1,9,8,3,4,2,5,6,7},
+        {8,5,9,7,6,1,4,2,3},
+        {4,2,6,8,5,3,7,9,1},
+        {7,1,3,9,2,4,8,5,6},
+        {9,6,1,5,3,7,2,8,4},
+        {2,8,7,4,1,9,6,3,5},
+        {3,4,5,2,8,6,1,7,9}};
+
+        int[,] arrayInvalidColumns = new int[9, 9]{
+        {1,2,3,4,5,6,7,8,9},
+        {1,2,3,4,5,6,7,8,9},
+        {1,2,3,4,5,6,7,8,9},
+        {1,2,3,4,5,6,7,8,9},
+        {1,2,3,4,5,6,7,8,9},
+        {1,2,3,4,5,6,7,8,9},
+        {1,2,3,4,5,6,7,8,9},
+        {1,2,3,4,5,6,7,8,9},
+        {1,2,3,4,5,6,7,8,9}};
+
+        int[,] arrayInvalid = new int[9, 9]{
+        {7,7,7,6,7,8,9,1,2},
+        {6,7,2,1,9,5,3,4,8},
+        {1,9,8,3,4,2,5,6,7},
+        {8,5,9,7,6,1,4,2,3},
+        {4,2,6,8,5,3,7,9,1},
+        {7,1,3,9,2,4,8,5,6},
+        {9,6,1,5,3,7,2,8,4},
+        {2,8,7,4,1,9,6,3,5},
+        {3,4,5,2,8,6,1,7,9}};
+
+
+
+
+        [TestMethod]
+        public void IsValid_True()
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+            bool expected = true;
+            bool actual = SudokuValidate.IsValid(array);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IsValid_False()
+        {
+            int[] array = { 1, 1, 3, 4, 5, 6, 7, 8, 9 };
+
+            bool expected = false;
+            bool actual = SudokuValidate.IsValid(array);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ValidateRows_With_Valid_Array()
+        {
+            SudokuValidate sudoku = new SudokuValidate(arrayValid);
+            bool expected = true;
+            bool actual = sudoku.ValidateRows();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ValidateColumns_With_Valid_Array()
+        {
+            SudokuValidate sudoku = new SudokuValidate(arrayValid);
+            bool expected = true;
+            bool actual = sudoku.ValidateColumns();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ValidateSquares_With_Valid_Array()
+        {
+            SudokuValidate sudoku = new SudokuValidate(arrayValid);
+            bool expected = true;
+            bool actual = sudoku.ValidateSquares();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ValidateRows_With_Invalid_Columns()
+        {
+            SudokuValidate sudoku = new SudokuValidate(arrayInvalidColumns);
+            bool expected = true;
+            bool actual = sudoku.ValidateRows();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ValidateColumns_With_Invalid_Columns()
+        {
+            SudokuValidate sudoku = new SudokuValidate(arrayInvalidColumns);
+            bool expected = false;
+            bool actual = sudoku.ValidateColumns();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ValidateSquares_With_Invalid_Columns()
+        {
+            SudokuValidate sudoku = new SudokuValidate(arrayInvalidColumns);
+            bool expected = false;
+            bool actual = sudoku.ValidateSquares();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ValidateRows_With_Invalid_Array()
+        {
+            SudokuValidate sudoku = new SudokuValidate(arrayInvalid);
+            bool expected = false;
+            bool actual = sudoku.ValidateRows();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ValidateColumns_With_Invalid_Array()
+        {
+            SudokuValidate sudoku = new SudokuValidate(arrayInvalid);
+            bool expected = false;
+            bool actual = sudoku.ValidateColumns();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ValidateSquares_With_Invalid_Array()
+        {
+            SudokuValidate sudoku = new SudokuValidate(arrayInvalid);
+            bool expected = false;
+            bool actual = sudoku.ValidateSquares();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
